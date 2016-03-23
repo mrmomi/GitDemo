8fc806cb202abb0b13c98e123663b7ef7eb84d8d
jgyhc <jgyhc@foxmail.com>
2016-03-23 09:06:12 +0800

    Initial Commit

---
+++ GirDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GirDemo
+//
+//  Created by rimi on 16/3/23.
+//  Copyright © 2016年 momi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GirDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GirDemo
+//
+//  Created by rimi on 16/3/23.
+//  Copyright © 2016年 momi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
